6f9f5bf31ca6ba122440b067dec47fb5927139d7
Jorge Rambla Gonzalez <yorch044zgz@gmail.com>
2020-01-30 18:54:49 +0100

    api pointer updated

--- app/config/web.js
+++ app/config/web.js
@@ -2,6 +2,6 @@ export default {
     URL: 'https://murcy.netlify.com/',
     //URL: 'http://192.168.1.33:19006/',
     //BACK: 'https://preunizar-30248-2019-murcy.herokuapp.com/api',
-    //BACK: 'http://localhost:9090/api',
-    BACK: 'http://murcyapi:9090/api'
+    BACK: 'http://localhost:9090/api',
+    //BACK: 'http://murcyapi:9090/api'
 };
